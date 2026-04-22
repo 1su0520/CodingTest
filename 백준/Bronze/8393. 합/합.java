5cfe415ba8bfd4db20f5b3a0f6fc1703f0295895
김서은 <s2642@e-mirim.hs.kr>
2026-04-22 09:13:19 +0900

    [Bronze V] Title: 합, Time: 164 ms, Memory: 17512 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String args[]){
+		Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int plus=0;
+        for (int i=1; i<=n; i++) plus+=i;
+        System.out.print(plus);
+    }
+}
