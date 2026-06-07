cd12ffec21a63662685f57c2d66f8c190f11e04b
김서은 <s2642@e-mirim.hs.kr>
2026-06-08 08:49:49 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n, int k) {
+    int answer = (n*12000)+(k*2000);
+    if(n>=10){
+        answer-=(n/10)*2000;
+    }
+    return answer;
+}
